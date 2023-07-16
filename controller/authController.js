de13dd8d6bdde5208d46c15d41ed2c5928cacb54
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-07-16 20:18:58 +0800

    login

--- controller/authController.js
+++ controller/authController.js
@@ -1,7 +1,7 @@
 const Users = require('../model/authModel')
 const bcrypt = require('bcryptjs')
 
-async function register(req, res) {
+const register = async (req, res) => {
   const { email, name, password } = req.body
 
   // Validate input
@@ -36,4 +36,35 @@ async function register(req, res) {
   }
 }
 
-module.exports = { register }
+const login = async (req, res) => {
+  const { email, password } = req.body
+  try {
+    // Validate input
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Please enter all fields' })
+    }
+
+    // Find user by email or username
+    const user = await Users.findOne({ email })
+    if (!user) {
+      return res.status(400).json({ message: "Email doesn't exist! " })
+    }
+
+    // Check password
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+      return res.status(400).json({ message: 'Email or password is incorrect' })
+    } else {
+      req.session.user = user
+      res.status(200).json({
+        message: 'Login Successfull',
+        user: req.session.user,
+      })
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: 'Login failed' })
+  }
+}
+
+module.exports = { register, login }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const userController = require('../controller/authController')
 
 router.post('/register', userController.register)
+router.post('/login', userController.login)
 
 module.exports = router
