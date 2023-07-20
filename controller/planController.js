23abf00be38808a1dcaaf6d6b43c2c36e4e553af
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-07-21 06:14:02 +0800

    UpdateFunc & Status

--- controller/planController.js
+++ controller/planController.js
@@ -95,4 +95,74 @@ const deletePlan = async (req, res) => {
       .json({ error: 'An error occurred while deleting the item.' })
   }
 }
-module.exports = { createPlan, viewPlan, deletePlan }
+const updatePlan = async (req, res) => {
+  const {
+    title,
+    departureDate,
+    arrivalDate,
+    destination,
+    distance,
+    companion,
+    thingsToBring,
+    notes,
+  } = req.body
+  const planId = req.params.id
+
+  try {
+    if (!planId) {
+      return res.status(400).json({ message: 'No Id Found!' })
+    }
+
+    const updatePlan = await Plan.findOneAndUpdate(
+      { _id: planId },
+      {
+        title,
+        departureDate,
+        arrivalDate,
+        destination,
+        distance,
+        companion,
+        thingsToBring,
+        notes,
+      }
+    )
+    if (!updatePlan) {
+      return res.status(404).json({ message: 'Item not found.' })
+    } else {
+      res
+        .status(200)
+        .json({ message: 'Item updated successfully.', updatePlan })
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: 'An error occurred while updating the item.' })
+  }
+}
+const updateStatus = async (req, res) => {
+  const { status } = req.body
+  const planId = req.params.id
+
+  try {
+    if (!planId) {
+      return res.status(400).json({ message: 'No Id Found!' })
+    }
+
+    const updatePlan = await Plan.findOneAndUpdate(
+      { _id: planId },
+      {
+        status,
+      }
+    )
+    if (!updatePlan) {
+      return res.status(404).json({ message: 'Item not found.' })
+    } else {
+      res.status(200).json({ message: 'Status updated successfully.' })
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: 'An error occurred while updating the status.' })
+  }
+}
+module.exports = { createPlan, viewPlan, deletePlan, updatePlan, updateStatus }
--- routes/planRoute.js
+++ routes/planRoute.js
@@ -5,5 +5,7 @@ const planController = require('../controller/planController')
 router.post('/create', planController.createPlan)
 router.get('/view', planController.viewPlan)
 router.delete('/delete/:id', planController.deletePlan)
+router.put('/update/:id', planController.updatePlan)
+router.put('/status/:id', planController.updateStatus)
 
 module.exports = router
