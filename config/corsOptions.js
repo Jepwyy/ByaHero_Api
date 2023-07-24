8176c22376b4d7ec3439a886a8762f4fc4603142
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-07-25 06:03:22 +0800

    corsUp

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -4,6 +4,9 @@ const allowedOrigins = [
   'http://localhost:5500',
   'http://127.0.0.1:5500',
   'http://127.0.0.1:8080',
+  'https://bya-hero.vercel.app',
+  'https://bya-hero-jepwyy.vercel.app',
+  'https://bya-hero-git-main-jepwyy.vercel.app',
 ]
 const corsOptions = {
   origin: (origin, callback) => {
