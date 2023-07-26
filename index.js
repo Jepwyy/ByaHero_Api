d4ea603bf9186fd0ef30941f79a23d8213151d55
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-07-27 05:22:55 +0800

    fixError

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000
 
 /* Middleware */
 app.use(express.json())
-app.use(cors(app.use(cors(corsOptions))))
+app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
 
