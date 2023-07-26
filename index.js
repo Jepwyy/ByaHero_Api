c67aea868877303fb8c23b227061b9e9367272f5
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-07-27 03:09:35 +0800

    cs

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-const session = require('express-session')
+const session = require('cookie-session')
 const corsOptions = require('./config/corsOptions')
-const MongoDBSessionStore = require('connect-mongodb-session')(session)
 require('dotenv').config()
 const port = process.env.PORT || 5000
-
-const store = new MongoDBSessionStore({
-  uri: process.env.ATLAS_URI, // MongoDB connection URI
-  collection: 'sessions', // MongoDB collection where sessions will be stored
-})
 
 /*Middleware*/
 app.use(express.json())
@@ -22,16 +16,12 @@ app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(
   session({
-    store: store,
     key: 'userId',
     secret: process.env.SESSION_SECRET_KEY,
     resave: false,
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7,
-      secure: true,
-      sameSite: 'none',
-      httpOnly: true,
     },
   })
 )
