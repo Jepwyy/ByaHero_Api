8d8af9bcd5d27e0f6fb95ec06280c01f147757dd
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-07-27 05:51:55 +0800

    cors-

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 require('dotenv').config()
-
+const corsOptions = require('./config/corsOptions')
 const app = express()
 const port = process.env.PORT || 5000
 
 /* Middleware */
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
 
