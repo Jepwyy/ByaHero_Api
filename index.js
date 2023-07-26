c199babbdd7615434612f55e83271fd7082c08f9
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-07-27 05:48:55 +0800

    updated

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const MongoDBStore = require('connect-mongodb-session')(session)
 const cors = require('cors')
-const corsOptions = require('./config/corsOptions')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 require('dotenv').config()
@@ -13,7 +12,7 @@ const port = process.env.PORT || 5000
 
 /* Middleware */
 app.use(express.json())
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -37,6 +36,8 @@ app.use(
     store: mongoDBStore,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7,
+      sameSite: 'None',
+      secure: true,
     },
   })
 )
