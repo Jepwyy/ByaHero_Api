b1970341b144dcfa863fb3f8ddfda598d438cc32
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-07-27 02:11:56 +0800

    ckiStore

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
 const corsOptions = require('./config/corsOptions')
+const MongoDBSessionStore = require('connect-mongodb-session')(session)
 require('dotenv').config()
 const port = process.env.PORT || 5000
+
+const store = new MongoDBSessionStore({
+  uri: process.env.ATLAS_URI, // MongoDB connection URI
+  collection: 'sessions', // MongoDB collection where sessions will be stored
+})
 
 /*Middleware*/
 app.use(express.json())
@@ -16,6 +22,7 @@ app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(
   session({
+    store: store,
     key: 'userId',
     secret: process.env.SESSION_SECRET_KEY,
     resave: false,
