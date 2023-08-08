1af9a9f7ca5984fce25a42368fa6f495de403711
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-08-09 01:45:36 +0800

    cks

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-const session = require('express-session')
+const session = require('cookie-session')
+
 const corsOptions = require('./config/corsOptions')
 require('dotenv').config()
 const port = process.env.PORT || 5000
